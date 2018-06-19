176ab016057961ef2179eb8f26abf13b2e3615aa
dr1zzt2 <dr1zzt2.NK@gmail.com>
2018-06-19 13:27:50 +0200

    start_game function continued

--- rpg_game.py
+++ rpg_game.py
@@ -12,3 +12,9 @@ def print_slow(str):
 def start_game():
     print_slow("Welcome!\n")
     time.sleep(1)
+    print_slow("You are a prisoner thrown into a very ancient dungeon.\n")
+    time.sleep(1)
+    print_slow("You have no objectives. You will never escape the pit.\n")
+    time.sleep(1)
+    print_slow("It's time to choose your class.\n")
+    pick_class()
